5829936821cd30286dbfdf4ff6023ff00998656e
Tucker Ledford <twl6@students.uwf.edu>
2024-02-22 15:09:05 -0600

    Finished Vehicle Class Code

--- ParkingFines.java
+++ ParkingFines.java
@@ -1,5 +1,17 @@
 public class ParkingFines {
-
     double fineAmount;
     String reasonForFine;
+
+    ParkingFines(double newFineAmount, String newReasonForFine) {
+        fineAmount = newFineAmount;
+        reasonForFine = newReasonForFine;
+    }
+
+    public double getFineAmount() {
+        return fineAmount;
+    }
+
+    public String getReasonForFine() {
+        return reasonForFine;
+    }
 }
