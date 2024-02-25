a25604fb47aa17c9c5cb671fdda0a8a177990731
DyeTry <d.tran.uni@gmail.com>
2024-02-25 10:22:57 -0600

    Began Parking Pass methods and added comments

--- src/main/java/com/team2/cen3031spring2024team2/ParkingManager.java
+++ src/main/java/com/team2/cen3031spring2024team2/ParkingManager.java
@@ -19,10 +19,29 @@ public class Parking_Manager {
          */
     }
 
+    public void login() {
+        /**
+         * Implement a way to login to the Parking Manager
+         * Will use users login credentials
+         */
+    }
+
+    /**
+     * Creates a Customer and adds object to customerList
+     *
+     * @param name The name of the Customer
+     * @param passwordString The Customer's password
+     */
     public void createCustomerAccount(String name, String passwordString) {
         customerList.add(new Customer(name, passwordString, customerList.size()));
     }
 
+    /**
+     * Creates an Employee and adds object to employeeList
+     *
+     * @param name The name of the Employee
+     * @param passwordString The Employee's password
+     */
     public void createEmployeeAccount(String name, String passwordString) {
         employeeList.add(new Employee(name, passwordString, employeeList.size()));
     }
@@ -30,6 +49,57 @@ public class Parking_Manager {
     public void registerVehicle(String make, String model, String color, String licensePlate) {
         /**
          * Implement a way to first identify who is registering the vehicle and then create the Vehicle object
+         */
+    }
+
+    public void updateVehicle(String make, String model, String color, String licensePlate) {
+        /**
+         * Implement a way to update vehicle information
+         * Might just use registerVehicle() again
+         */
+    }
+
+    /**
+     * A method to find Customer information
+     * @param id The Customer's ID number
+     * @return Returns the Customer's Information
+     */
+    public boolean findCustomer(int id) {
+
+        for (Customer info : customerList) {
+
+            if (info.getID() == id) {
+                return info;
+            }
+        }
+
+        return false;
+    }
+
+    public void buyParkingPass() {
+        /**
+         * Implement a way to buy a parking pass
+         */
+    }
+
+    public void updateParkingPass() {
+        /**
+         * Implement a way to update/modify the user's parking pass
+         * This method will be used if the user wants to upgrade or downgrade their pass
+         */
+    }
+
+    public void renewParkingPass() {
+        /**
+         * Implement a way to check the parking passes expiration date and allow the customer to increase the timer
+         * Most likely use java.time to check expiration date
+         */
+    }
+
+    public void cancelParkingPass() {
+        /**
+         * Thought Experiment:
+         * Implement a way to allow the customer to cancel their parking pass coverage
          */
     }
 }
