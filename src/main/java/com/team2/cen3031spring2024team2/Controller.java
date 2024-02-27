ef62f93012e6139f6346822e06d07d0d32d88ee4
J5osully <jjo6@students.uwf.edu>
2024-02-27 13:26:36 -0600

    Created initial drafts of GUIs and functioning menu buttons

--- src/main/java/com/team2/cen3031spring2024team2/Controller.java
+++ src/main/java/com/team2/cen3031spring2024team2/Controller.java
@@ -1,14 +1,61 @@
 package com.team2.cen3031spring2024team2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 public class Controller {
     @FXML
     private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public void switchToBasePane(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("employeeBasePane.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToMapPane(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("employeeMapPane.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToUserPane(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("employeeUserSearchPane.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToPassPane(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("employeePassPane.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToIssuePane(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("employeeIssuesPane.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
