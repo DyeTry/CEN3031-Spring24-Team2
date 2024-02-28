df874e49a72ea90bc3a83c6e1a6a62be0ad1f8bd
J5osully <jjo6@students.uwf.edu>
2024-02-28 15:18:35 -0600

    Modified and added GUI panes, added button functionality for all panes

--- src/main/java/com/team2/cen3031spring2024team2/Controller.java
+++ src/main/java/com/team2/cen3031spring2024team2/Controller.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 
@@ -16,6 +18,12 @@ public class Controller {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private MenuButton searchTypeMenu;
+
+    private String searchTitle;
+
+    private String searchEntry;
 
 
     public void switchToBasePane(ActionEvent event) throws IOException {
@@ -56,5 +64,50 @@ public class Controller {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void switchToUserSearchResultPane(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("userSearchResultPane.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onPlateAssignment(ActionEvent event) throws IOException {
+        //Throw success/failure message
+    }
+
+    public void chooseIdSearch(ActionEvent event) throws IOException {
+        //Change redirect of Submit button and rename "Choose Search Type" appropriately
+        searchTitle = "Search by ID";
+        searchTypeMenu.setText(searchTitle);
+    }
+
+    public void choosePlateSearch(ActionEvent event) throws IOException {
+        //Change redirect of Submit button and rename "Choose Search Type" appropriately
+        searchTitle = "Search by Plate";
+        searchTypeMenu.setText(searchTitle);
+    }
+
+    public void choosePassSearch(ActionEvent event) throws IOException {
+        //Change redirect of Submit button and rename "Choose Search Type" appropriately
+        searchTitle = "Search by Pass";
+        searchTypeMenu.setText(searchTitle);
+    }
+
+    public void chooseFineSearch(ActionEvent event) throws IOException {
+        //Change redirect of Submit button and rename "Choose Search Type" appropriately
+        searchTitle = "Search by Fine";
+        searchTypeMenu.setText(searchTitle);
+    }
+
+    public void onSearchEntry(ActionEvent event) throws IOException {
+        //Set searchEntry equal to entered search value
+    }
+
+    public void onUserSearch(ActionEvent event) throws IOException {
+        //Redirect to search results page
+        switchToUserSearchResultPane(event);
     }
 }
