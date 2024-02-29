c16aaee11682134b359f3272b983d40ea045122d
Oscar Garcia <og21@students.uwf.edu>
2024-02-29 10:08:18 -0600

    Database classes for cust info and employee info

---
+++ CustInfo.java
@@ -0,0 +1,83 @@
+public class CustInfo {
+    private String name;
+    private String carMake;
+    private String carModel;
+    private String carColor;
+    private String licencePlate;
+    private String passExpirationDate;
+    private String username;
+    private String password;
+    private int employeeID;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public String getCarMake() {
+        return carMake;
+    }
+
+    public void setCarMake(String carMake) {
+        this.carMake = carMake;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public String getPassExpirationDate() {
+        return passExpirationDate;
+    }
+
+    public void setPassExpirationDate(String passExpirationDate) {
+        this.passExpirationDate = passExpirationDate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+}
