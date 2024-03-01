ae0cb0bdb151a703e18450cede7735e759956b9d
Rikki <rjc61@students.uwf.edu>
2024-03-01 15:56:54 -0600

    Save

--- src/main/java/com/team2/cen3031spring2024team2/Controller.java
+++ src/main/java/com/team2/cen3031spring2024team2/Controller.java
@@ -1,14 +1,40 @@
 package com.team2.cen3031spring2024team2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.stage.Stage;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
-
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MenuButton searchTypeMenu;
+
+    private String searchTitle;
+
+    private String searchEntry;
+
+
+
+    public void switchToParkingFines(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Parking Fine.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
+
+
 }
