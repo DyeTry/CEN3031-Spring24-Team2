0e2823ba504c9a14f2f48c594327c3c122663b84
Rikki <rjc61@students.uwf.edu>
2024-03-02 18:00:22 -0600

    Draft before comparison

--- src/main/java/com/team2/cen3031spring2024team2/Main.java
+++ src/main/java/com/team2/cen3031spring2024team2/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("CustomerBase.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("CustomerMap.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Customer");
         stage.setScene(scene);
