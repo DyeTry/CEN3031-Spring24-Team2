e3d7a11d27a8afcae27941a063e8d50f51a9ff67
Oscar Garcia <og21@students.uwf.edu>
2024-04-21 21:13:11 -0500

    test

--- src/main/java/com/team2/cen3031spring2024team2/Controller.java
+++ src/main/java/com/team2/cen3031spring2024team2/Controller.java
@@ -538,7 +538,7 @@ public class Controller implements Initializable {
     }
 
     private CustomerInfo permanentCustomer;
-    public void initUser(CustomerInfo customer) { //jeremiah is lovely
+    public void initUser(CustomerInfo customer) {
         permanentCustomer = customer;
         System.out.println(customer.getName() + " found");
     }
